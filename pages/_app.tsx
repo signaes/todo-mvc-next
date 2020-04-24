f29ef0bd06248f4f89cbe83bc2a664e1683cafe1
Thiago Oliveira <thiago@signaes.com>
2020-04-24 09:22:22 -0300

    Add SASS support

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../assets/styles/app.scss'
+import { AppProps } from 'next/app'
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default App
