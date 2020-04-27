992eba95eab05029a8edad203b89d1e9acac418a
Thiago Oliveira <thiago@signaes.com>
2020-04-26 23:37:31 -0300

    Add todo class

---
+++ models/todo.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+export interface TodoInterface {
+  id: string;
+  title: string;
+  done: boolean;
+}
+
+export default class Todo {
+  public static async getTodos() {
+    try {
+      const { data } = await axios.get('/api/todos');
+
+      return data;
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+  }
+
+  public static async newTodo(title: string) {
+    try {
+      const { data } = await axios.post('/api/todos', { title });
+
+      return data;
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+  }
+
+  public static async updateTodo({ id, done }: { id: string; done: boolean }) {
+    try {
+      const { data } = await axios.patch(`/api/todo/${id}`, { done });
+
+      return data;
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+  }
+
+  public static async deleteTodo(id: string) {
+    try {
+      const { data } = await axios.delete(`/api/todo/${id}`);
+
+      return data;
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+  }
+}
